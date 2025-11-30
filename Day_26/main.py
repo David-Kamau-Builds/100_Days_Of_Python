ca69eb5d65745b711ea904225983a342ca79885f
David kamau <david.washington.kamau@gmail.com>
2025-11-30 23:56:08 +0300

    Week_5: Initial Commit

--- Day_26/main.py
+++
@@ -1,8 +0,0 @@
-import pandas
-phonetic_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
-
-phonetic_dict = {row.letter:row.code for (index, row) in phonetic_data_frame.iterrows()}
-
-word = input("What word do you want to convert?: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
