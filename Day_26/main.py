1627d477677f29b04d8e5675813236b7f5d4a660
David Washington Kamau <113665931+David-Kamau-Builds@users.noreply.github.com>
2025-12-02 21:11:22 +0300

    Day_26 Updated: "Added exception to improve wrong input handling"

--- Day_26/main.py
+++ Day_26/main.py
@@ -1,8 +1,17 @@
 import pandas
+
 phonetic_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-phonetic_dict = {row.letter:row.code for (index, row) in phonetic_data_frame.iterrows()}
+phonetic_dict = {row.letter: row.code for (index, row) in phonetic_data_frame.iterrows()}
 
-word = input("What word do you want to convert?: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("What word do you want to convert?: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters A–Z are allowed. Try again.")
+        return generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
